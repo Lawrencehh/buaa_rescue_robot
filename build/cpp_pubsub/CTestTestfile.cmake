9ca653824d076949c8301db3d715920391f8149e
Lawrencehh <sy1607511@buaa.edu.cn>
2023-09-01 12:31:15 +0800

    C++  talker and listener test

---
+++ build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rescue-robot/buaa_rescue_robot/src/cpp_pubsub
+# Build directory: /home/rescue-robot/buaa_rescue_robot/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
