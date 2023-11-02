53a6b1f883865da4e7ae1d14a6b9e26ce2da16b1
Lawrencehh <sy1607511@buaa.edu.cn>
2023-11-02 16:05:02 +0800

    1

--- src/serial/src/serial_PullPushSensors_1.cpp
+++ src/serial/src/serial_PullPushSensors_1.cpp
@@ -77,8 +77,10 @@ public:
     {
         // 尝试初始化串口，如果出错，记录错误信息。
         try {
-            serial_port_ = std::make_shared<asio::serial_port>(io_, "/dev/ttyPullPushSensors1");  // 这里的路径需要根据您的设备进行更改
-            serial_port_->set_option(asio::serial_port::baud_rate(115200));  // 设置波特率
+            serial_port_1 = std::make_shared<asio::serial_port>(io_, "/dev/ttyPullPushSensors1");  // 这里的路径需要根据您的设备进行更改
+            serial_port_1->set_option(asio::serial_port::baud_rate(115200));  // 设置波特率
+            serial_port_2 = std::make_shared<asio::serial_port>(io_, "/dev/ttyPullPushSensors2");  // 这里的路径需要根据您的设备进行更改
+            serial_port_2->set_option(asio::serial_port::baud_rate(115200));  // 设置波特率
         }
         catch (const std::exception &e) {
             RCLCPP_ERROR(this->get_logger(), "Could not open serial port: %s", e.what());
@@ -114,10 +116,29 @@ private:    // 私有成员函数和变量
     asio::streambuf write_buffer_;  // 新添加的写缓冲区
     rclcpp::Publisher<buaa_rescue_robot_msgs::msg::SensorsMessageMasterDevicePullPushSensors>::SharedPtr publisher_;   // add a publisher of SensorsMessage
 
-    // ROS 2 Humble版本的异步写入封装函数
-    void async_write_to_serial(const std::vector<uint8_t>& data_to_write)
+    // ROS 2 Humble版本的异步写入封装函数,serial_port_1
+    void async_write_to_serial_1(const std::vector<uint8_t>& data_to_write)
     {
-        asio::async_write(*serial_port_, asio::buffer(data_to_write),
+        asio::async_write(*serial_port_1, asio::buffer(data_to_write),
+            [this](const asio::error_code& error, std::size_t bytes_transferred)
+            {
+                // 检查是否有错误
+                if (!error) 
+                {
+
+                } 
+                else 
+                {
+                    RCLCPP_ERROR(this->get_logger(), "Write Error: %s", error.message().c_str());
+                }
+            }
+        );
+    }
+
+    // ROS 2 Humble版本的异步写入封装函数,serial_port_2
+    void async_write_to_serial_2(const std::vector<uint8_t>& data_to_write)
+    {
+        asio::async_write(*serial_port_2, asio::buffer(data_to_write),
             [this](const asio::error_code& error, std::size_t bytes_transferred)
             {
                 // 检查是否有错误
@@ -187,8 +208,8 @@ private:    // 私有成员函数和变量
         received_modbus_frame_.clear();
         received_modbus_frame_.resize(256);
 
-        // 异步读取串口数据
-        serial_port_->async_read_some(asio::buffer(received_modbus_frame_, 256),
+        // 异步读取串口数据serial_port_1
+        serial_port_1->async_read_some(asio::buffer(received_modbus_frame_, 256),
             [this](const asio::error_code& error, std::size_t bytes_transferred)
             {
                 // 调整received_modbus_frame_的大小以匹配实际接收到的字节数
@@ -224,20 +245,13 @@ private:    // 私有成员函数和变量
 
                         for (size_t i = 0; i < 6; i++)
                         {
-                            pull_push_sensors_1[i] = pull_push_sensors_1_part[i];
+                            pull_push_sensors_1[2*i] = pull_push_sensors_1_part[i];
                         }
 
                         // 移除这34字节(including the ending 0xCFFCCCFF)及之前的字节
                         data_buffer_.erase(data_buffer_.begin(), it_pull_push_encorder + 34);
                     }
-                    
-                    auto msg = buaa_rescue_robot_msgs::msg::SensorsMessageMasterDevicePullPushSensors();       
-                    msg.pull_push_sensors_1 = pull_push_sensors_1;
-                    publisher_->publish(msg);
-
                     received_modbus_frame_.clear();
-                    // 递归调用以持续接收
-                    start_receive();
                 }
                 else 
                 {
@@ -245,6 +259,65 @@ private:    // 私有成员函数和变量
                 }
             }
         );
+
+        // 异步读取串口数据serial_port_2
+        serial_port_2->async_read_some(asio::buffer(received_modbus_frame_, 256),
+            [this](const asio::error_code& error, std::size_t bytes_transferred)
+            {
+                // 调整received_modbus_frame_的大小以匹配实际接收到的字节数
+                received_modbus_frame_.resize(bytes_transferred);
+
+                // 检查是否有错误
+                if (!error) 
+                {
+                    // 将接收到的数据添加到数据缓存区
+                    data_buffer_.insert(data_buffer_.end(), received_modbus_frame_.begin(), received_modbus_frame_.end());
+
+                    // 打印data_buffer_内容
+                    // std::string msg_str = "";
+                    // for (auto &byte : data_buffer_) {
+                    //     msg_str += "0x" + to_string(static_cast<int>(byte)) + " ";
+                    // }
+                    // RCLCPP_INFO(this->get_logger(), "Receive message: %s", msg_str.c_str());  
+
+
+                    // 创建一个包含数据头的vector
+                    std::vector<uint8_t> pull_push_sensors_header = {0xFE,0x01,0x50,0xFF};
+                    // 搜索数据头
+                    auto it_pull_push_encorder = std::search(data_buffer_.begin(), data_buffer_.end(), pull_push_sensors_header.begin(), pull_push_sensors_header.end());
+
+                    // 如果找到了数据头，并且有足够的字节用于完整的34字节帧 (excluding the ending 0xCFFCCCFF)
+                    if (it_pull_push_encorder != data_buffer_.end() && std::distance(it_pull_push_encorder, data_buffer_.end()) >= 34)
+                    {
+                        // 提取30字节帧
+                        std::vector<uint8_t> frame(it_pull_push_encorder+1, it_pull_push_encorder + 30);
+
+                        // 处理Modbus帧并获取elevator_counter
+                        pull_push_sensors_1_part = process_modbus_frame_for_pull_push_sensors(frame);
+
+                        for (size_t i = 0; i < 6; i++)
+                        {
+                            pull_push_sensors_1[2*i+1] = pull_push_sensors_1_part[i];
+                        }
+
+                        // 移除这34字节(including the ending 0xCFFCCCFF)及之前的字节
+                        data_buffer_.erase(data_buffer_.begin(), it_pull_push_encorder + 34);
+                    }
+                    received_modbus_frame_.clear();
+                }
+                else 
+                {
+                    RCLCPP_ERROR(this->get_logger(), "Receive Error: %s", error.message().c_str());
+                }
+            }
+        );
+
+        auto msg = buaa_rescue_robot_msgs::msg::SensorsMessageMasterDevicePullPushSensors();       
+        msg.pull_push_sensors_1 = pull_push_sensors_1;
+        publisher_->publish(msg);
+
+        // 递归调用以持续接收
+        start_receive();
 
     }
 
@@ -263,7 +336,8 @@ private:    // 私有成员函数和变量
             // }
             // RCLCPP_INFO(this->get_logger(), "Sending frame: %s", frame_str.c_str());
             // 通过串口发送数据帧
-            async_write_to_serial(frame);
+            async_write_to_serial_1(frame);
+            async_write_to_serial_2(frame); 
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(10));  // 延迟10毫秒   
     }
@@ -280,14 +354,16 @@ private:    // 私有成员函数和变量
         // }
         // RCLCPP_INFO(this->get_logger(), "Sending frame: %s", frame_str.c_str());
         // 通过串口发送数据帧
-        async_write_to_serial(frame);     
+        async_write_to_serial_1(frame);     
+        async_write_to_serial_2(frame); 
     }
 
 
     rclcpp::TimerBase::SharedPtr timer_;    // 定时器
 
     rclcpp::Subscription<buaa_rescue_robot_msgs::msg::ControlMessage>::SharedPtr subscription_;  //订阅器
-    std::shared_ptr<asio::serial_port> serial_port_;    // 串口对象
+    std::shared_ptr<asio::serial_port> serial_port_1;    // 串口对象
+    std::shared_ptr<asio::serial_port> serial_port_2;    // 串口对象
     asio::io_service io_;   // ASIO I/O服务
     std::vector<uint8_t> modbus_frame_; // 存储Modbus协议帧
     
